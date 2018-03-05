3b0d67c4e94afedada85bc26b074ee4a23868472
Hazel <hazelhleung@gmail.com>
2018-03-04 20:19:44 -0800

    a/b testing

---
+++ app/public/js/abclick.js
@@ -0,0 +1,24 @@
+'use strict';
+
+// Call this function when the page loads (the "ready" event)
+$(document).ready(function() {
+	initializePage();
+});
+
+/*
+ * Function that is called when the document is ready.
+ */
+function initializePage() {
+	$(".click").click(likeClick);
+
+
+}
+
+function likeClick(e){
+	e.preventDefault();
+	console.log("location button clicked");
+	ga("send", "event", "locationclick", "click");
+
+	//route to chorelist
+	window.location.href = '../chorelist.html';
+}
